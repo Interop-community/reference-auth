68825f02308b6f0dad5c414691d857213e599b19
Travis Cummings <travis@interopion.com>
2018-02-25 19:01:42 -0700

    new pipeline

--- ci/ci-2-build-project.sh
+++ ci/ci-2-build-project.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-mvn -V -B -f ../pom.xml -s settings.xml deploy -P DEPLOY-HSPC,hspc-nexus
+mvn -V -B -f ../pom.xml -s ../settings.xml deploy -P DEPLOY-HSPC,hspc-nexus
+rc=$?
+if [[ ${rc} -ne 0 ]] ; then
+  echo 'could not perform tests'; exit $rc
+fi
+
 rm ../reference-auth-server-webapp/target/*-sources.jar
 rm ../reference-auth-server-webapp/target/*-javadoc.jar
