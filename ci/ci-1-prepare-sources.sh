524ec57cfe6a6fdf7c63b00bd3a7e48b07500650
Travis Cummings <travis@interopion.com>
2018-02-25 21:09:13 -0700

    new pipeline

--- ci/ci-1-prepare-sources.sh
+++ ci/ci-1-prepare-sources.sh
@@ -7,6 +7,4 @@ jq ".containerDefinitions[0].logConfiguration.options.\"awslogs-group\" = \"/ecs
 jq "(.containerDefinitions[0].environment[] | select(.name == \"JASYPT_ENCRYPTOR_PASSWORD\") | .value) |= \""${ENC_PW_TEST}"\"" ${TEMPLATE_FILE} > tmp.json && mv tmp.json ${TEMPLATE_FILE}
 jq "(.containerDefinitions[0].environment[] | select(.name == \"SPRING_PROFILES_ACTIVE\") | .value) |= \"${SPRING_PROFILES_ACTIVE}\"" ${TEMPLATE_FILE} > tmp.json && mv tmp.json ${TEMPLATE_FILE}
 
-echo "outside: ${ENC_PW_TEST}"
-
 cat ${TEMPLATE_FILE}
