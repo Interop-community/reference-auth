8eb90627ef87a32e5fa08bfc45f9cc05101398b6
Travis Cummings <travis@interopion.com>
2018-02-25 19:14:17 -0700

    new pipeline

--- docker/build.sh
+++ docker/build.sh
@@ -13,4 +13,4 @@ docker \
   --build-arg WAR_FILE=reference-auth-server-webapp/target/*.war \
   --build-arg JETTY_RUNNER_FILE=reference-auth-server-webapp/target/dependency/jetty-runner.jar \
   --build-arg JETTY_CONFIG_FILE=reference-auth-server-webapp/src/main/resources/jetty.xml  \
-  .
+  ..
