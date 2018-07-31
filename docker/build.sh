c5c19e96a0da18d361e3bd01d86f4dec2a1f3902
Travis Cummings <travis@interopion.com>
2018-07-31 11:28:03 -0600

    Added docker-compose.yml

--- docker/build.sh
+++ docker/build.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-echo "Note: this should be run from the project root directory"
-
 version="latest"
 
 if [ $# -gt 0 ]; then
   version=$1
 fi
 
+# files must be in a folder or subfolder
+mkdir -p target
+cp ../reference-auth-server-webapp/target/*.war target
+cp ../reference-auth-server-webapp/target/dependency/jetty-runner.jar target
+cp ../reference-auth-server-webapp/src/main/resources/jetty.xml target
+
 docker \
-  build -t hspconsortium/auth:${PROJECT_VERSION} \
-  --build-arg WAR_FILE=reference-auth-server-webapp/target/*.war \
-  --build-arg JETTY_RUNNER_FILE=reference-auth-server-webapp/target/dependency/jetty-runner.jar \
-  --build-arg JETTY_CONFIG_FILE=reference-auth-server-webapp/src/main/resources/jetty.xml  \
-  ..
+  build -t hspconsortium/auth:${version} \
+  --build-arg WAR_FILE=target/*.war \
+  --build-arg JETTY_RUNNER_FILE=target/jetty-runner.jar \
+  --build-arg JETTY_CONFIG_FILE=target/jetty.xml  \
+  .
