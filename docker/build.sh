24a42c3f8456a81df6741cd59a23249982586950
Travis Cummings <travis@interopion.com>
2018-07-31 13:54:54 -0600

    docker-compose v3

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-version="latest"
-
+tag="hspconsortium/auth:latest"
 if [ $# -gt 0 ]; then
-  version=$1
+  tag=$1
 fi
 
 # files must be in a folder or subfolder
@@ -13,7 +12,7 @@ cp ../reference-auth-server-webapp/target/dependency/jetty-runner.jar target
 cp ../reference-auth-server-webapp/src/main/resources/jetty.xml target
 
 docker \
-  build -t hspconsortium/auth:${version} \
+  build -t $tag \
   --build-arg WAR_FILE=target/*.war \
   --build-arg JETTY_RUNNER_FILE=target/jetty-runner.jar \
   --build-arg JETTY_CONFIG_FILE=target/jetty.xml  \
