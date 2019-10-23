eb83260d5fc574abc43362e88d277f94992c52a7
Shilpy <shilpy@interopion.com>
2019-10-23 14:12:07 -0600

    minor changes

--- reference-auth-server-webapp/src/main/java/org/hspconsortium/platform/authorization/repository/impl/KeycloakUserInfoRepository.java
+++ reference-auth-server-webapp/src/main/java/org/hspconsortium/platform/authorization/repository/impl/KeycloakUserInfoRepository.java
@@ -1,7 +1,5 @@
 package org.hspconsortium.platform.authorization.repository.impl;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -25,33 +23,33 @@ public class KeycloakUserInfoRepository extends PersonaUserInfoRepository {
     public UserInfo getRealUserByUsername(String username) {
 
         UserInfo userInfo = new DefaultUserInfo();
-//        userInfo.setSub("4fa1fb37-13ab-4019-bef2-ec3ce53aa91e");
-//        userInfo.setPreferredUsername("name");
-//        userInfo.setEmail("email@email.com");
-//        userInfo.setName("displayName");
-
-        // TODO: populate userInfo
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        try {
-            HttpGet request = new HttpGet(userInfoEndpointUrl);
-            // TODO: set headers (token)
-            CloseableHttpResponse response = httpClient.execute(request);
-            HttpEntity entity = response.getEntity();
-            String responseString = EntityUtils.toString(entity, StandardCharsets.UTF_8);
-            JSONParser parser = new JSONParser();
-            JSONObject oicUserInfo = (JSONObject) parser.parse(responseString);
-
-            if (entity != null) {
-//                userInfo.setSub(oicUserInfo.get("sub").toString());
-//                userInfo.setPreferredUsername(oicUserInfo.get("username").toString());
-//                userInfo.setEmail(oicUserInfo.get("email").toString());
-//                userInfo.setName(oicUserInfo.get("name").toString());
-            }
-        } catch (Exception e) {
-
-        }
+        userInfo.setSub("4fa1fb37-13ab-4019-bef2-ec3ce53aa91e");
+        userInfo.setPreferredUsername("name");
+        userInfo.setEmail("email@email.com");
+        userInfo.setName("displayName");
+//
+//        // TODO: populate userInfo
+//        CloseableHttpClient httpClient = HttpClients.createDefault();
+//        try {
+//            HttpGet request = new HttpGet(userInfoEndpointUrl);
+//            // TODO: set headers (token)
+//            CloseableHttpResponse response = httpClient.execute(request);
+//            HttpEntity entity = response.getEntity();
+//            if (entity != null) {
+//                String responseString = EntityUtils.toString(entity, StandardCharsets.UTF_8);
+//                JSONParser parser = new JSONParser();
+//                JSONObject oicUserInfo = (JSONObject) parser.parse(responseString);
+//
+////                userInfo.setSub(oicUserInfo.get("sub").toString());
+////                userInfo.setPreferredUsername(oicUserInfo.get("username").toString());
+////                userInfo.setEmail(oicUserInfo.get("email").toString());
+////                userInfo.setName(oicUserInfo.get("name").toString());
+//            }
+//        } catch (Exception e) {
+//
+//        }
 
         return userInfo;
     }
-
 }
+
